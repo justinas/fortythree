262c48a2256d8e9a4173845ea8a32f8059460aa6
Justinas Stankevicius <justinas@justinas.me>
2015-06-26 14:16:21 +0300

    memcpy

---
+++ utils.rs
@@ -0,0 +1,11 @@
+/// Straight from rlibc
+#[no_mangle]
+pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
+                            n: usize) -> *mut u8 {
+    let mut i = 0;
+    while i < n {
+        *dest.offset(i as isize) = *src.offset(i as isize);
+        i += 1;
+    }
+    return dest;
+}
